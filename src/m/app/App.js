ce902743fb44126cba36535a896bd6934fbf2b62
Stefan Haack <shaack@gmail.com>
2019-07-16 12:33:10 +0200

    added App

---
+++ src/m/app/App.js
@@ -0,0 +1,14 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/m
+ * License: MIT, see file 'LICENSE'
+ */
+
+export class App {
+
+    constructor(props = {}) {
+        this.props = props
+        this.components = []
+    }
+
+}
---
+++ src/m/app/Component.js
@@ -0,0 +1,16 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/m
+ * License: MIT, see file 'LICENSE'
+ */
+
+export class Component {
+
+    constructor(app, props = {}) {
+        this.app = app
+        this.props = props
+        this.state = {}
+        this.components = []
+    }
+
+}
