06ce54a499d62292308423019ed9e3cd43042e8e
Stefan Haack <shaack@gmail.com>
2021-07-10 16:26:03 +0200

    added EncryptionUtils.js

---
+++ src/cm-web-modules/utils/EncryptionUtils.js
@@ -0,0 +1,18 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-web-modules
+ * License: MIT, see file 'LICENSE'
+ */
+
+export class EncryptionUtils {
+
+    static createKey(length = 32) {
+        let key = ""
+        const iterations = length / 10
+        for (let i = 0; i < iterations; i++) {
+            key += Math.floor(1000000000000000 + Math.random() * 9000000000000000).toString(36).substr(0, 10)
+        }
+        return key.substr(0, length)
+    }
+
+}
---
+++ test/TestEncryptionUtils.js
@@ -0,0 +1,20 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-web-modules
+ * License: MIT, see file 'LICENSE'
+ */
+import {describe, it, assert} from "../node_modules/teevi/src/teevi.js"
+import {EncryptionUtils} from "../src/cm-web-modules/utils/EncryptionUtils.js"
+
+describe("EncryptionUtils", function () {
+
+    it("should generate keys", function () {
+        assert.equal(EncryptionUtils.createKey(1).length, 1)
+        assert.equal(EncryptionUtils.createKey(10).length, 10)
+        assert.equal(EncryptionUtils.createKey(11).length, 11)
+        assert.equal(EncryptionUtils.createKey(32).length, 32)
+        assert.equal(EncryptionUtils.createKey(128).length, 128)
+        assert.true(EncryptionUtils.createKey(32) !== EncryptionUtils.createKey(32))
+    })
+
+})
