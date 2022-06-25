2672885e76396b846ec1f3e8dec5c5de3b673d25
shaack <shaack@gmail.com>
2022-06-25 14:08:40 +0200

    Added PromiseQueue.js

--- src/cm-web-modules/observed/Observed.js
+++ src/cm-web-modules/observed/Observed.js
@@ -1,6 +1,7 @@
 /**
- * Author: Stefan Haack (https://shaack.com)
- * Date: 2021-08-30
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-web-modules
+ * License: MIT, see file 'LICENSE'
  */
 
 export function Observed(target) {
---
+++ test/TestPromiseQueue.js
@@ -0,0 +1,52 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-web-modules
+ * License: MIT, see file 'LICENSE'
+ */
+
+import {describe, it} from "../node_modules/teevi/src/teevi.js"
+import {PromiseQueue} from "../src/cm-web-modules/promise-queue/PromiseQueue.js"
+
+describe("PromiseQueue", function () {
+    it("Should queue some promises and execute them in the right Order", function () {
+        const queue = new PromiseQueue()
+        queue.enqueue(
+            () => new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve()
+                }, 500)
+            }).then(() => (
+                console.log("1")
+            ))
+        )
+        queue.enqueue(() => new Promise((resolve) => {
+                resolve()
+            }).then(() => (
+                console.log("2")
+            ))
+        )
+        queue.enqueue(() => new Promise((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, 500)
+            }).then(() => (
+                console.log("3")
+            ))
+        )
+        queue.enqueue(() => new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve()
+                }, 100)
+            }).then(() => (
+                console.log("4")
+            ))
+        )
+        queue.enqueue(() => new Promise((resolve) => {
+                resolve()
+            }).then(() => (
+                console.log("5")
+            ))
+        )
+
+    })
+})
