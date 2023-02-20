30d40cf193119fa7bb1dd4e6daef5ba4a29288aa
shaack <shaack@gmail.com>
2023-02-20 12:00:25 +0100

    stopwatch modify time

--- src/cm-web-modules/stopwatch/Stopwatch.js
+++ src/cm-web-modules/stopwatch/Stopwatch.js
@@ -14,16 +14,17 @@ export class Stopwatch {
             tickResolution: 100 // milliseconds of the time resolution
         }
         Object.assign(this.props, props)
-        this.startDate = null
+        this.startDate = new Date()
         this.endDate = null
         this.dateAtLatestStart = null
         this.secondsExpiredTillLastPause = 0.0
         this.secondsExpiredSinceLastStart = 0.0
+        this.additionalSeconds = 0
         this.timer = null
     }
 
     secondsExpired() {
-        return this.secondsExpiredTillLastPause + this.secondsExpiredSinceLastStart
+        return this.secondsExpiredTillLastPause + this.secondsExpiredSinceLastStart + this.additionalSeconds
     }
 
     start() {
@@ -37,7 +38,7 @@ export class Stopwatch {
                 this.tick()
             }, this.props.tickResolution)
             this.props.onStateChanged(this.running())
-            this.props.onTimeChanged(this.secondsExpiredTillLastPause)
+            this.props.onTimeChanged(this.secondsExpired())
         }
     }
 
@@ -46,7 +47,7 @@ export class Stopwatch {
             clearInterval(this.timer)
             this.timer = null
             this.endDate = new Date()
-            this.secondsExpiredTillLastPause = this.secondsExpired()
+            this.secondsExpiredTillLastPause = this.secondsExpiredTillLastPause + this.secondsExpiredSinceLastStart
             this.secondsExpiredSinceLastStart = 0
             this.props.onStateChanged(this.running())
         }
@@ -55,10 +56,16 @@ export class Stopwatch {
     reset() {
         this.secondsExpiredSinceLastStart = 0
         this.secondsExpiredTillLastPause = 0
+        this.additionalSeconds = 0
         this.startDate = new Date()
         this.dateAtLatestStart = new Date()
         this.props.onTimeChanged(this.secondsExpired())
         this.props.onStateChanged(this.running())
+    }
+
+    addSeconds(seconds) {
+        this.additionalSeconds =  this.additionalSeconds + seconds
+        this.props.onTimeChanged(this.secondsExpired())
     }
 
     tick() {
